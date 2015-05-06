06cc966b366dd8c5b632f55b94b322ce26e94509
Rob Bearman <robbear@hyperfine.com>
2015-05-06 12:02:45 -0700

    Begin work on Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,4 @@
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
+  grunt.loadTasks('grunt/tasks');
+};
---
+++ grunt/tasks/setup.js
@@ -0,0 +1,73 @@
+var exec = require("child_process").exec;
+
+module.exports = function(grunt) {
+
+  var repositories = [
+    'basic-web-components',
+    'basic-element',
+    'basic-autosize-textarea'
+  ];
+
+  grunt.initConfig({
+    shell: {
+      'git-clone': {
+        command: function(repo) {
+          return 'git clone git@github.com:basic-web-components/' + repo + '.git';
+        },
+
+        options: {
+          execOptions: {
+            cwd: 'repos'
+          }
+        }
+      },
+
+      'git-pull': {
+        command: function(repo) {
+          return 'git pull';
+        }
+      },
+
+      'change-dir': {
+        command: function(dir) {
+          return 'cd ' + dir;
+        }
+      },
+    }
+  });
+
+  grunt.registerTask('create-repos-dir', function() {
+    if (grunt.file.exists('repos')) {
+      return;
+    }
+
+    grunt.file.mkdir('repos');
+  });
+
+  grunt.registerTask('clone-repos', function() {
+    for (var i = 0; i < repositories.length; i++) {
+      var repo = repositories[i];
+
+      if (!grunt.file.exists('repos/' + repo)) {
+        grunt.task.run(['shell:git-clone:' + repo]);
+      }
+    }
+  });
+
+  grunt.registerTask('update-repos', function() {
+    for (var i = 0; i < repositories.length; i++) {
+      var repo = repositories[i];
+
+      grunt.task.run(['shell:change-dir:repos/' + repo, 'shell:git-pull', 'shell:change-dir:../..']);
+    }
+  });
+
+  grunt.registerTask('test', function() {
+    grunt.task.run(['create-repos-dir', 'clone-repos']);
+  });
+
+  grunt.registerTask('test1', function() {
+    grunt.task.run(['create-repos-dir', 'clone-repos', 'update-repos']);
+  })
+
+};
